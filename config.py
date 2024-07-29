8272e9d44653216a2150e6a947b3d724527c0d04
umar2111k <138141263+umar2111k@users.noreply.github.com>
2024-07-29 10:47:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-
-
 import re
 import os
 from os import environ
@@ -25,7 +23,7 @@ BOT_USERNAME = environ.get("BOT_USERNAME", "batches_lecture_provider_bot") # wit
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
-CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
+CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://yefixi9254:hu8ldGaA0ledNFiO@cluster0.af4er1c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
