9b089e47043c560a9ba6adb9f6135310a5a658dc
Umar565 <titaniumgamingr@gmail.com>
2024-11-18 19:37:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "23475322"))
 API_HASH = environ.get("API_HASH", "e00e5cebf073df8baba7db34ea0ebdc9")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8061533845:AAEBYVnIkcMw2-qRzmwThbegolH1HNw3yk0")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8061533845:AAGG2uyg3QAEL2t1nnlzUyAMuyQm2bU0UnY")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/04aa01515e5c9fca02854.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6170050819 5868554818 6169992190').split()]
